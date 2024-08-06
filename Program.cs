0f084288fbf1aec80919258258e46df09844b3b5
gregorygregory745 <psaccesargregory@gmail.com>
2024-08-05 23:36:06 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace arreglos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[10];
+
+            Console.WriteLine("Por favor, ingresa 10 números desordenados:");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("Número " + (i + 1) + ": ");
+                numeros[i] = int.Parse(Console.ReadLine());
+            }
+
+            Array.Sort(numeros);
+
+            Console.WriteLine("\nNúmeros ordenados:");
+            foreach (int numero in numeros)
+            {
+                Console.WriteLine(numero);
+            }
+        }
+    }
+}
+           
